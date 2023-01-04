6c1159d96b025896c06220494c63f5b4a403f79e
Leo Ling <leocabbage2008@gmail.com>
2023-01-03 22:24:38 -0500

    scroll bar dissappear

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,65 +1,66 @@
-import React, { useState } from "react";
-import space from ".././assets/images/space.jpg";
-import ".././assets/css/Navbar.css";
-import classNames from "classnames";
+import React, { useState } from 'react';
+import space from '.././assets/images/space.jpg';
+import '.././assets/css/Navbar.css';
+import classNames from 'classnames';
 
 export default function Navbar() {
   const [sidebar, setSidebar] = useState(false);
   const toggle = () => {
-    document.getElementById("root").style.overflow =
-      document.body.style.overflow = sidebar ? "auto" : "hidden";
+    document.body.style.overflow = document.getElementById(
+      'root'
+    ).style.overflowY = sidebar ? 'scroll' : 'hidden';
     setSidebar(!sidebar);
   };
   return (
     <>
-      <div className={classNames("tint", { "active-tint": sidebar })} />
+      <div className={classNames('tint', { 'active-tint': sidebar })} />
       <nav>
-        <a href="/" className="logo-link">
-          <img className="logo" src={space} alt="" />
+        <a href='/' className='logo-link'>
+          <img className='logo' src={space} alt='' />
         </a>
-        <div className="nav-container">
-          <ul className="nav-links">
+        <div className='nav-container'>
+          <ul className='nav-links'>
             <li>
-              <a href="/">Home</a>
+              <a href='/'>Home</a>
             </li>
             <li>
-              <a href="/Details">Details</a>
+              <a href='/Details'>Details</a>
             </li>
             <li>
-              <a href="/sources">Sources</a>
+              <a href='/sources'>Sources</a>
             </li>
           </ul>
           <button
-            className={classNames("burger", { "active-burger": sidebar })}
+            className={classNames('burger', { 'active-burger': sidebar })}
             onClick={toggle}
           >
-            <div className="line1"></div>
-            <div className="line2"></div>
-            <div className="line3"></div>
+            <div className='line1'></div>
+            <div className='line2'></div>
+            <div className='line3'></div>
           </button>
         </div>
       </nav>
-      <div className={classNames("sidebar", { active: sidebar })}>
-        <ul className="sidebar-links">
+      <div className={classNames('sidebar', { active: sidebar })}>
+        <ul className='sidebar-links'>
           <li>
-            <a href="/ref">Ref</a>
+            <a href='/ref'>Ref</a>
           </li>
           <li>
             <hr />
           </li>
           <li>
-            <a href="/">Home</a>
+            <a href='/'>Home</a>
           </li>
           <li>
             <hr />
           </li>
           <li>
-            <a href="/">About</a>
+            <a href='/'>About</a>
           </li>
         </ul>
       </div>
       <div
-        className={classNames("sidecover", { "sidecover-active": sidebar })}
+        className={classNames('sidecover', { 'sidecover-active': sidebar })}
       />
     </>
   );
