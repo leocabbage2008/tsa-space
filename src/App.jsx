cc278271566c7712423f843f96e0794bb2acfd57
Leo Ling <leocabbage2008@gmail.com>
2023-01-05 22:30:05 -0500

    hash router for ghpages

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,27 @@
-import React from "react";
+import React from 'react';
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Routes,
   Route,
   Navigate,
-} from "react-router-dom";
-import "./assets/css/index.css";
-import SourcesPage from "./pages/SourcesPage.jsx";
-import ReferencePage from "./pages/ReferencePage.jsx";
-import ErrorPage from "./pages/ErrorPage.jsx";
-import HomePage from "./pages/HomePage.jsx";
-import DetailsPage from "./pages/DetailsPage";
+} from 'react-router-dom';
+import './assets/css/index.css';
+import SourcesPage from './pages/SourcesPage.jsx';
+import ReferencePage from './pages/ReferencePage.jsx';
+import ErrorPage from './pages/ErrorPage.jsx';
+import HomePage from './pages/HomePage.jsx';
+import DetailsPage from './pages/DetailsPage';
 //front or home page should be for promotion
 
 const App = () => (
   <Router>
     <Routes>
-      <Route exact path="/" element={<HomePage />} />
-      <Route exact path="/home" element={<Navigate to="/" />} />
-      <Route exact path="/sources" element={<SourcesPage />} />
-      <Route exact path="/ref" element={<ReferencePage />} />
-      <Route exact path="/details" element={<DetailsPage />} />
-      <Route path="*" element={<ErrorPage />} />
+      <Route exact path='/' element={<HomePage />} />
+      <Route exact path='/home' element={<Navigate to='/' />} />
+      <Route exact path='/sources' element={<SourcesPage />} />
+      <Route exact path='/ref' element={<ReferencePage />} />
+      <Route exact path='/details' element={<DetailsPage />} />
+      <Route path='*' element={<ErrorPage />} />
     </Routes>
   </Router>
 );
