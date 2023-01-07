661b5bd7a309568319e5ea30016c1345541d99a6
Ian <austinyan54@gmail.com>
2023-01-07 16:53:47 -0500

    deleted the dumb kids

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,28 +1,28 @@
-import React from 'react';
-import Hero from '../components/Hero';
-import Layout from '../components/Layout';
-import space from '../assets/images/space.webp';
+import React from "react";
+import Hero from "../components/Hero";
+import Layout from "../components/Layout";
+import space from "../assets/images/space.webp";
 export default function HomePage() {
-  return (
-    <Layout>
-      <Hero text='hmm quite spacey' image={space} />
-      <div className='content'>
-        {/* <h1>Home Page</h1> */}
-        <ul>
-          <li>
-            <a href='/error'>error</a>
-          </li>
-          <li>
-            <a href='/sources'>sources</a>
-          </li>
-          <li>
-            <a href='/ref'>references</a>
-          </li>
-          <li>
-            <a href='/details'>details</a>
-          </li>
-        </ul>
-      </div>
-    </Layout>
-  );
+    return (
+        <Layout>
+            <Hero text="hmm quite spacey" image={space} />
+            {/* <div className="content">
+                <h1>Home Page</h1>
+                <ul>
+                    <li>
+                        <a href="/error">error</a>
+                    </li>
+                    <li>
+                        <a href="/sources">sources</a>
+                    </li>
+                    <li>
+                        <a href="/ref">references</a>
+                    </li>
+                    <li>
+                        <a href="/details">details</a>
+                    </li>
+                </ul>
+            </div> */}
+        </Layout>
+    );
 }
