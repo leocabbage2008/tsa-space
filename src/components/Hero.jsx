e6c917f6e105cbc79d797f56cf24334b29d52ec6
Ian <austinyan54@gmail.com>
2023-01-07 19:17:09 -0500

    h

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ import "../assets/css/Hero.css";
 export default function Hero({ image, text }) {
     return (
         <div className="hero">
-            <img src={image} alt="" />
+            <img id="banner" src={image} alt="" />
             {/* <h1>{text}</h1> */}
         </div>
     );
