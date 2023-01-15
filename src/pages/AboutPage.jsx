536744c138762e6d2e88db6f870e9de47fbd0d1f
Ian <austinyan54@gmail.com>
2023-01-14 22:37:32 -0500

    b

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,4 +1,11 @@
 import React from "react";
 import Layout from "../components/Layout";
-import video from "../assets/images/world.mp4";
 import "../assets/css/AboutPage.css";
+
+export default function HomePage() {
+    return (
+        <Layout>
+            <p></p>
+        </Layout>
+    );
+}
