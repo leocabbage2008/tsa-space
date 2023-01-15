2f90b8b03a4dee3853ed50864f62dfb8e6180b4d
Leo Ling <leocabbage2008@gmail.com>
2023-01-15 14:43:27 -0500

    logo

--- src/pages/SourcesPage.jsx
+++ src/pages/SourcesPage.jsx
@@ -8,30 +8,42 @@ export default function SourcesPage() {
       <div className='content'>
         <h1>Sources Page</h1>
         <div id='sources'>
-          <ul>
-            <li>
-              <Source title='Title Page Video'>
-                <a href='https://pixabay.com/videos/world-earth-nasa-planet-globe-2/'>
-                  https://pixabay.com/videos/world-earth-nasa-planet-globe-2/
-                </a>
-                <p>(Public Domain)</p>
-              </Source>
-              <Source title='Title Page Video'>
-                <a href='https://pixabay.com/photos/cape-canaveral-florida-space-shuttle-79355/'>
-                  https://pixabay.com/photos/cape-canaveral-florida-space-shuttle-79355/
-                </a>
-                <p>(Public Domain)</p>
-              </Source>
-            </li>
-            <li>
-              <Source title='Information'>
-                <a href='https://school.eb.com/levels/high/article/Titan/72639'>
-                https://school.eb.com/levels/high/article/Titan/72639
-                </a>
-                <p>(DataBase)</p>
-              </Source>
-            </li>
-          </ul>
+          <Source title='Title Page Video'>
+            <a href='https://pixabay.com/videos/world-earth-nasa-planet-globe-2/'>
+              https://pixabay.com/videos/world-earth-nasa-planet-globe-2/
+            </a>
+            <p>(Public Domain)</p>
+          </Source>
+          <Source title='Title Page Video'>
+            <a href='https://pixabay.com/photos/cape-canaveral-florida-space-shuttle-79355/'>
+              https://pixabay.com/photos/cape-canaveral-florida-space-shuttle-79355/
+            </a>
+            <p>(Public Domain)</p>
+          </Source>
+          <Source title='Information'>
+            <a href='https://school.eb.com/levels/high/article/Titan/72639'>
+              https://school.eb.com/levels/high/article/Titan/72639
+            </a>
+            <p>(DataBase)</p>
+          </Source>
+          <Source title='Icons'>
+            <a href='https://feathericons.com'>https://feathericons.com</a>
+            <p>(Public Domain)</p>
+          </Source>
+          <Source title='Fonts'>
+            <a href='https://fonts.google.com/specimen/Merriweather/about'>
+              Merriweather
+            </a>
+            <p>(SIL Open Font License)</p>
+            <br />
+            <a href='https://fonts.google.com/specimen/Mulish/about'>Mulish</a>
+            <p>(SIL Open Font License)</p>
+            <br />
+            <a href='https://fonts.google.com/specimen/Staatliches/about'>
+              Staatliches
+            </a>
+            <p>(SIL Open Font License)</p>
+          </Source>
         </div>
       </div>
     </Layout>
