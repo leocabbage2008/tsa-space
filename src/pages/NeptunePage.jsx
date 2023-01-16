a4efd38d02e8dfbf777daaebdc323122720c14f6
godliestguy <94765256+godliestguy@users.noreply.github.com>
2023-01-15 22:04:49 -0500

    neptuune

--- src/pages/NeptunePage.jsx
+++ src/pages/NeptunePage.jsx
@@ -7,17 +7,15 @@ export default function HomePage() {
             <div className="content">
                 <h1>The God of Water and Seas</h1>
                 <h2>Neptune his one of the best places to visit in our solar system. Based on our path, you could see it's small rings, which are pretty cool and 
-                    rarely seen. You could also go onto the surface and play sone hockey as the surface is ice. However, you cannot play for too long, or you will 
-                    freeze... that is without our best spacesuits. With our latest technologies, you could enjoy the surface for up to 5 hours without having to 
-                    worry about not having air, or being frozen. The views of the topography are truly unmatched! 
+                    rarely seen. You could also book a tour of Neptune's great dark spot, by shuttle, or visit one of it's moons!
                 </h2>
                 <h1>
                     --------------------------------------------------------------------------------------
                 </h1>
                 <h1>Our Facilities</h1>
-                <h2>With over 20 locations on Neptune, we provide places safe from it's massive storms for you to relax, sleep, and recover from the cold temperatures.
+                <h2>With over 20 locations on Neptune's moons and in Neptune's orbit, we provide places safe from it's massive storms for you to relax, sleep, and recover from the cold temperatures.
                     Inside our facilities, we have hot spas for your warmth and relaxation after a long day of fun activities. We also have many other fun things like malls, 
-                    gyms, and living quarters. 
+                    gyms, and living quarters throughout. 
                 </h2>
             </div>
         </Layout>
