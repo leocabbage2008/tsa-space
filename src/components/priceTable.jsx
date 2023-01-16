6dd03f6e533bea8bea6c98d20b474372f5aae098
Leo Ling <leocabbage2008@gmail.com>
2023-01-15 22:43:09 -0500

    table

--- src/components/priceTable.jsx
+++ src/components/priceTable.jsx
@@ -1,56 +1,28 @@
-import React from "react";
-import Layout from "../components/Layout";
-import "../assets/css/Price_table.css";
+import React from 'react';
+import '../assets/css/PriceTable.css';
 
-export default function priceTable({ l1, l2, l3, p1, p2, p3 }) {
-    return (
-        <Layout>
-            <div class="container">
-                <div class="price-row">
-                    <div class="price-col col1">
-                        <p>Basic</p>
-                        <h3>
-                            {p1} <span>/day</span>
-                        </h3>
-                        {/* <ul>
-                            <li>C programming basic</li>
-                            <li>Variables in c</li>
-                            <li>Function in c</li>
-                            <li>Basic programs in c</li>
-                        </ul> */}
-                        {l1}
-                        <button>Buy Now</button>
-                    </div>
-                    <div class="price-col col2">
-                        <p>Standard</p>
-                        <h3>
-                            {p2} <span>/day</span>
-                        </h3>
-                        {/* <ul>
-                            <li>Algorithms in c</li>
-                            <li>Data structures in c</li>
-                            <li>Medium+hard level programs</li>
-                            <li>High quality study material</li>
-                        </ul> */}
-                        {l2}
-                        <button>Buy Now</button>
-                    </div>
-                    <div class="price-col col3">
-                        <p>Premium</p>
-                        <h3>
-                            {p3} <span>/day</span>
-                        </h3>
-                        {/* <ul>
-                            <li>C programming basic</li>
-                            <li>Algorithms in c</li>
-                            <li>Data structures in c</li>
-                            <li>All level programs</li>
-                        </ul> */}
-                        {l3}
-                        <button>Buy Now</button>
-                    </div>
-                </div>
+export default function PriceTable({ data }) {
+  console.log(data);
+  return (
+    <div className='plan-container'>
+      {data.map((item, i) => {
+        return (
+          <div className='price-row' key={i}>
+            <div className={`price-col col${i + 1}`}>
+              <p>{item.title}</p>
+              <h3>
+                {item.price} <span>/day</span>
+              </h3>
+              <ul>
+                {item.list.map((item, i) => {
+                  return <li key={i}>{item}</li>;
+                })}
+              </ul>
+              <button>Buy Now</button>
             </div>
-        </Layout>
-    );
+          </div>
+        );
+      })}
+    </div>
+  );
 }
--- src/pages/MarsPage.jsx
+++ src/pages/MarsPage.jsx
@@ -1,49 +1,61 @@
-import React from "react";
-import Layout from "../components/Layout";
-import "../assets/css/Titan.css";
-import priceTable from "../components/priceTable";
+import React from 'react';
+import Layout from '../components/Layout';
+import '../assets/css/Titan.css';
+import PriceTable from '../components/PriceTable';
 export default function HomePage() {
-    return (
-        <Layout>
-            <div className="content">
-                <h1>The Red Planet</h1>
-                <h2>Mars has many great places for you to visit. You could take a hike on one of the Solar System's largest volcanoes, Olympus Mons for starters.
-                    Or you can travel to the poles and look at the frozen water that could've hade life billions of years ago in a different age.
-                    If you aren't a bumpy terrain kind of person we offer trips on vehicles that go through some of Mars' 'Seas'.
-                </h2>
-                <br/>
-                <h1>Our Facilities</h1>
-                <h2>With 26 locations on Mars, we provide places safe from Mars dust storms for you to relax, sleep, and recover from Mars's low gravity.
-                    Also, we provide seasonal events on Mars as it's Seasons also have a cycle just like Earth.
-                </h2>
-                <br/>
-                <h1>Close to Home</h1>
-                <h2>
-                    Mars has many features that make it similar to Earth. It's
-                    day night cycle and seasonal cycle are close to Earth's
-                    allowing you do only have to adapt to the gravity. Of source
-                    there are some key differences including it's lack of
-                    Atmosphere, Magnetic field, low temperatures, and toxic soil
-                    but we provide suits that almost make it seem like you are
-                    back on Earth.
-                </h2>
-            </div>
-            {priceTable(<ul>
-                            <li>C programming basic</li>
-                            <li>Variables in c</li>
-                            <li>Function in c</li>
-                            <li>Basic programs in c</li>
-                        </ul>), <ul>
-                            <li>C programming basic</li>
-                            <li>Variables in c</li>
-                            <li>Function in c</li>
-                            <li>Basic programs in c</li>
-                        </ul>, <ul>
-                            <li>C programming basic</li>
-                            <li>Variables in c</li>
-                            <li>Function in c</li>
-                            <li>Basic programs in c</li>
-                        </ul>, "$10", "$20", "$30")}
-        </Layout>
-    );
+  const data = [
+    {
+      title: 'basic',
+      price: '$50',
+      list: ['1 day', '1 person'],
+    },
+    {
+      title: 'standard',
+      price: '$100',
+      list: ['1 day', '1 person', '1 vehicle'],
+    },
+    {
+      title: 'premium',
+      price: '$150',
+      list: ['1 day', '1 person', '1 vehicle', '1 hotel'],
+    },
+  ];
+  return (
+    <Layout>
+      <div className='content'>
+        <PriceTable data={data} />
+        <h1>The Red Planet</h1>
+        <h2>
+          Mars has many great places for you to visit. You could take a hike on
+          one of the Solar System's largest volcanoes, Olympus Mons for
+          starters. Or you can travel to the poles and look at the frozen water
+          that could've hade life billions of years ago in a different age. If
+          you aren't a bumpy terrain kind of person we offer trips on vehicles
+          that go through some of Mars' 'Seas'.
+        </h2>
+        <h1>
+          --------------------------------------------------------------------------------------
+        </h1>
+        <h1>Our Facilities</h1>
+        <h2>
+          With 26 locations on Mars, we provide places safe from Mars dust
+          storms for you to relax, sleep, and recover from Mars's low gravity.
+          Also, we provide seasonal events on Mars as it's Seasons also have a
+          cycle just like Earth.
+        </h2>
+        <h1>
+          --------------------------------------------------------------------------------------
+        </h1>
+        <h1>Close to Home</h1>
+        <h2>
+          Mars has many features that make it similar to Earth. It's day night
+          cycle and seasonal cycle are close to Earth's allowing you do only
+          have to adapt to the gravity. Of source there are some key differences
+          including it's lack of Atmosphere, Magnetic field, low temperatures,
+          and toxic soil but we provide suits that almost make it seem like you
+          are back on Earth.
+        </h2>
+      </div>
+    </Layout>
+  );
 }
