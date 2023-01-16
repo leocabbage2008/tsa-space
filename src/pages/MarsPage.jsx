ad7ed3e16f9f02ed576852565c099957a4d57356
William <whz11@scasd.org>
2023-01-15 22:47:00 -0500

    goddammit leo

--- src/pages/MarsPage.jsx
+++ src/pages/MarsPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Layout from '../components/Layout';
 import '../assets/css/Titan.css';
-import PriceTable from '../components/PriceTable.jsx';
+import PriceTable from '../components/priceTable.jsx';
 export default function HomePage() {
   const data = [
     {
