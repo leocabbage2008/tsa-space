613e6fd2186f78e952d9b6b224e2bc5cb9801f40
William <whz11@scasd.org>
2023-01-15 22:20:08 -0500

    removed '-' from destination pages

--- src/pages/MarsPage.jsx
+++ src/pages/MarsPage.jsx
@@ -10,16 +10,12 @@ export default function HomePage() {
                     Or you can travel to the poles and look at the frozen water that could've hade life billions of years ago in a different age.
                     If you aren't a bumpy terrain kind of person we offer trips on vehicles that go through some of Mars' 'Seas'.
                 </h2>
-                <h1>
-                    --------------------------------------------------------------------------------------
-                </h1>
+                <br/>
                 <h1>Our Facilities</h1>
                 <h2>With 26 locations on Mars, we provide places safe from Mars dust storms for you to relax, sleep, and recover from Mars's low gravity.
-                    Also, we provide seasonal events on Mars as it's Seasons also have a cycle just like Earth. 
+                    Also, we provide seasonal events on Mars as it's Seasons also have a cycle just like Earth.
                 </h2>
-                <h1>
-                    --------------------------------------------------------------------------------------
-                </h1>
+                <br/>
                 <h1>Close to Home</h1>
                 <h2>Mars has many features that make it similar to Earth. It's day night cycle and seasonal cycle are close to Earth's
                     allowing you do only have to adapt to the gravity. Of source there are some key differences including it's lack of Atmosphere, Magnetic field,
