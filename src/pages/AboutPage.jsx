7a3e69bc3f26a764379c1ab79bdbc0c07c5fa670
Leo <leocabbage2008@gmail.com>
2023-04-12 21:24:41 -0400

    about

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,40 +1,33 @@
 import React from 'react';
 import Layout from '../components/Layout';
 import '../assets/css/AboutPage.css';
+import banner from '../assets/images/aboutusbanner.png';
 
 export default function AboutPage() {
   return (
     <Layout>
-      <div className='content'>
-        <h1 className='about-us-title'>ABOUT US</h1>
-        <h2>
-          Our space tourism company, MSP (short for "Monke Space Programs"), is
-          dedicated to providing our customers with the ultimate space travel
-          experience. We offer a wide range of space travel options, from
-          suborbital joyrides to extended orbital vacations at other planets. Our
-          state-of-the-art spacecrafts are designed with safety and comfort in
-          mind, and our team of experienced pilots and space experts ensure that
-          our customers receive all the necessary training and instruction before
-          and during their flight.
-        </h2>
-
-        <br />
-        <br />
-        <h2>
-          From the moment you leave the Earth's atmosphere, you'll be able to
-          witness the breathtaking views of our planet, the stars, and the vast
-          expanse of space. Our goal is to provide an unforgettable and
-          life-changing experience, and we pride ourselves on making space
-          travel safe, comfortable, and accessible to everyone.
-        </h2>
-        <br />
-        <br />
-        <h2>
-          Our reputation as a leader in the space tourism industry is built on
-          our commitment to providing our customers with a truly unique and
-          unparalleled experience. So join us and embark on a journey of a
-          lifetime, as you explore the mysteries of space with MSP.
-        </h2>
+      <div className='banner-container'>
+        <img className='banner' src={banner} alt='' />
+        <div className='content'>
+          <h1 className='about-us-title'>ABOUT US</h1>
+          <h2>
+            Our space tourism company, MSP ("Monke Space Programs"), is
+            dedicated to providing our customers with the ultimate space travel
+            experience. We offer a wide range of space travel options, from
+            suborbital joyrides to extended orbital vacations at other planets.
+          </h2>
+          <br />
+          <h2>
+            From the moment you leave the Earth's atmosphere, you'll be able to
+            witness the breathtaking views of our planet, the stars, and the
+            vast expanse of space.
+          </h2>
+          <br />
+          <h2>
+            So join us and embark on a journey of a lifetime, as you explore the
+            mysteries of space with MSP.
+          </h2>
+        </div>
       </div>
     </Layout>
   );
